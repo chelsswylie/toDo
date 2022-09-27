7715a5660390e49a0dd337dd74f12221ff49146d
Chelsea <chelsswylie@gmail.com>
2022-09-27 19:58:36 -0400

    set up skeleton for react router dom, react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
-import React, { useState } from "react";
-import { TodoListItem } from "./molecule/listItem";
-import { Todo } from "./types/listTypes";
-
-const initialTodos: Todo[] = [
-  {
-    text: "Walk the dog",
-    complete: false,
-  },
-  {
-    text: "Write app",
-    complete: true,
-  },
-];
+import React from "react";
+import { FormPage } from "./pages/formPage";
+import { ToDoPage } from "./pages/toDoListPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
-
-  const toggleCheckbox = (selectedTodo: Todo) => {
-    const newTodos = todos.map((i) => {
-      if (i === selectedTodo) {
-        return {
-          ...i,
-          complete: !i.complete,
-        };
-      }
-      return i;
-    });
-    setTodos(newTodos);
-  };
-
   return (
-    <ul>
-      <TodoListItem todo={todos[0]} toggleTodo={toggleCheckbox} />
-      <TodoListItem todo={todos[1]} toggleTodo={toggleCheckbox} />
-    </ul>
+    <Router>
+      <div className="navigation_container">
+        <p>Route to the pages</p>
+      </div>
+      <Routes>
+        <Route path="/" element={<FormPage />} />
+        <Route path="/ToDo" element={<ToDoPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/formPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormComponent } from "../organism/form";
+
+interface Props {}
+
+export const FormPage: React.FC<Props> = ({}) => {
+  return (
+    <div>
+      <FormComponent />
+    </div>
+  );
+};
